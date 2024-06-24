6faa03322d58c578750b6df1b83cb5b371849319
Amrit <amritsinghal2211@gmail.com>
2024-06-24 11:53:07 +0530

    till left Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,32 @@
+'use client'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 function Sidebar({user}: SiderbarProps) {
+  const pathname = usePathname()
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
             <Link href= "/" className='mb-12 cursor-pointer items-center gap-2'>
-            <Image src= '/icons/logo.svg' width={34} height={34}alt='Horizon logo' className='size-[124px] max-xl:size-14'/> 
+            <Image src= '/icons/logo.svg' width={34} height={34} alt='Horizon logo' className='size-[24px] max-xl:size-14'/> 
+            <h1 className='sidebar-logo'>Horizon</h1>
             </Link>
+            {sidebarLinks.map((item)=>{
+              const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+              return (
+                <Link href={item.route} key = {item.label}
+                  className = {cn('sidebar-link' ,{'bg-bank-gradient':isActive})}>
+                    <div className="relative size-6">
+                    <Image src={item.imgURL} alt={item.label}  fill/>
+                    </div>
+                </Link>
+              )
+            }
+          )}
         </nav>
     </section>
   )
