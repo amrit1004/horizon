68be0c0464a34f25639213cab46a85eb82e2e2e5
Amrit <amritsinghal2211@gmail.com>
2024-06-24 16:38:13 +0530

    added mobilenav

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ function Sidebar({user}: SiderbarProps) {
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-            <Link href= "/" className='mb-12 cursor-pointer items-center gap-2'>
+            <Link href= "/" className='mb-12 cursor-pointer items-center gap-2 flex'>
             <Image src= '/icons/logo.svg' width={34} height={34} alt='Horizon logo' className='size-[24px] max-xl:size-14'/> 
             <h1 className='sidebar-logo'>Horizon</h1>
             </Link>
@@ -21,8 +21,11 @@ function Sidebar({user}: SiderbarProps) {
                 <Link href={item.route} key = {item.label}
                   className = {cn('sidebar-link' ,{'bg-bank-gradient':isActive})}>
                     <div className="relative size-6">
-                    <Image src={item.imgURL} alt={item.label}  fill/>
+                    <Image src={item.imgURL} alt={item.label}  fill className= {cn({'brightness-[3]':isActive})}/>
                     </div>
+                    <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
                 </Link>
               )
             }
