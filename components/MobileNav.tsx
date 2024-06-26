283a5358a44f8543a7c6f0020f19c1b789289c15
Amrit <amritsinghal2211@gmail.com>
2024-06-26 08:54:37 +0530

    left sidebar completed

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -32,15 +32,15 @@ function MobileNav({ user }: MobileNavProps) {
                     {sidebarLinks.map((item)=>{
               const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
               return (
+                <SheetClose asChild key ={item.route}>
                 <Link href={item.route} key = {item.label}
-                  className = {cn('sidebar-link' ,{'bg-bank-gradient':isActive})}>
-                    <div className="relative size-6">
-                    <Image src={item.imgURL} alt={item.label}  fill className= {cn({'brightness-[3]':isActive})}/>
-                    </div>
-                    <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                  className = {cn('mobilenav-sheet_close w-full' ,{'bg-bank-gradient':isActive})}>
+                    <Image src={item.imgURL} alt={item.label} height={20} width={20}  className= {cn({'brightness-[3]':isActive})}/>
+                    <p className={cn("text-16 font-semibold text-black-2", { "text-white": isActive })}>
                 {item.label}
               </p>
                 </Link>
+                </SheetClose> 
               )
             }
           )}
@@ -50,6 +50,7 @@ function MobileNav({ user }: MobileNavProps) {
         </SheetContent>
       </Sheet>
     </section>
+    
   );
 }
 
