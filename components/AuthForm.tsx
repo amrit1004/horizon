cbbf8fa96401902bb6c867329fcdde71d8769568
Amrit <amritsinghal2211@gmail.com>
2024-07-02 16:55:44 +0530

    sign in done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import { Input } from "./ui/input";
 import {
   Form,
@@ -16,52 +16,95 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import Custominput from "./Custominput";
 import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const AuthForm = ({ type }: { type: string }) => {
-    const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const form = useForm<z.infer<typeof authFormSchema>>({
     resolver: zodResolver(authFormSchema),
     defaultValues: {
-       email: "",
-       password: ''
+      email: "",
+      password: "",
     },
-  })
+  });
   function onSubmit(values: z.infer<typeof authFormSchema>) {
-
-    console.log(values)
+    setIsLoading(true);
+    console.log(values);
+    setIsLoading(false);
   }
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-5 md:gap-8">
-      <Link href= "/" className='cursor-pointer items-center gap-1  flex'>
-            <Image src= '/icons/logo.svg' width={34} height={34} alt='Horizon logo' /> 
-            <h1 className='text-26 font-ibm-plex- text-black-1'>Horizon</h1>
-            </Link>
-            <div className="flex flex-col gap-1 md:gap-3">
-                <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
-                    {user ? 'Link Account':type === 'sign-in' ?'Sign In':'Sign Up'}
-                    <p className="text-16 font-normal text-gray-600">
-                        {user ? 'Link your account to get started':'Please enter your details'}
-                    </p>
-                </h1>
-            </div>
+        <Link href="/" className="cursor-pointer items-center gap-1  flex">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+          />
+          <h1 className="text-26 font-ibm-plex- text-black-1">Horizon</h1>
+        </Link>
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? "Link your account to get started"
+                : "Please enter your details"}
+            </p>
+          </h1>
+        </div>
       </header>
       {user ? (
         <div className="flex flex-col gap-4"></div>
-      ):(
+      ) : (
         <>
-        <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <Custominput control = {form.control}   name = 'email' label="Email" placeholder="Enter your Email"/>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <Custominput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your Email"
+              />
 
-        <Custominput control = {form.control}   name = 'password' label="Password" placeholder="Enter your Password"/>
-
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+              <Custominput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your Password"
+              />
+              <Button type="submit" disabled={isLoading} className="form-btn">
+                {isLoading ? (
+                  <>
+                    <Loader2 size={20} className="animate-spin" /> &nbsp;
+                    Loading...
+                  </>
+                ) : type === "sign-in" ? (
+                  "Sign In"
+                ) : (
+                  "Sign Up"
+                )}
+              </Button>
+            </form>
+          </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign up" : "Sign in"}
+            </Link>
+          </footer>
         </>
       )}
     </section>
--- components/Custominput.tsx
+++ components/Custominput.tsx
@@ -26,7 +26,9 @@ const Custominput = ({control , name , label , placeholder}
                <div className="flex w-full flex-col">
                 <FormControl>
                     <Input placeholder={placeholder}
-                    className="input-class"{...field}/>
+                    className="input-class" type = {name==='password'? 'password':'text'}
+                    {...field} />
+
                 </FormControl>
                 <FormMessage className = "form-message mt-2" />
                </div>
